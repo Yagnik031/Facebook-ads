1bd9e467f52aeab4260844b8b06197bac196a30b
Yagnik031 <yagnik.itcodehelp@gmail.com>
2022-04-08 13:37:24 +0530

    ff

--- src/web.ts
+++ src/web.ts
@@ -12,4 +12,62 @@ export class FacebookAdsWeb extends WebPlugin implements FacebookAdsPlugin {
     console.log('create banner', _options);
     return _options;
   }
+
+  async initialize(): Promise<void> {
+    console.log('initialize');
+  }
+
+  async targetSettings(): Promise<void> {
+    console.log('targetSettings');
+  }
+
+  async trackingAuthorizationStatus(): Promise<any> {
+    return {
+      status: 'authorized',
+    };
+  }
+
+  async showBanner(options: any): Promise<void> {
+    console.log('showBanner', options);
+  }
+
+  // Hide the banner, remove it from screen, but can show it later
+  async hideBanner(): Promise<void> {
+    console.log('hideBanner');
+  }
+
+  // Resume the banner, show it after hide
+  async resumeBanner(): Promise<void> {
+    console.log('resumeBanner');
+  }
+
+  // Destroy the banner, remove it from screen.
+  async removeBanner(): Promise<void> {
+    console.log('removeBanner');
+  }
+
+  async prepareInterstitial(options: any): Promise<any> {
+    console.log('prepareInterstitial', options);
+    return {
+      adUnitId: options.adId,
+    };
+  }
+
+  async showInterstitial(): Promise<void> {
+    console.log('showInterstitial');
+  }
+
+  async prepareRewardVideoAd(options: any): Promise<any> {
+    console.log(options);
+    return {
+      adUnitId: options.adId,
+    };
+  }
+
+  async showRewardVideoAd(): Promise<any> {
+    return {
+      type: '',
+      amount: 0,
+    };
+  }
 }
